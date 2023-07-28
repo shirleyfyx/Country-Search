03adcffcb57787f77bcf8a11d7a05fc30f65ee93
Shirley Fang <fangyixuan0123@gmail.com>
2023-07-27 23:25:49 -0400

    Set up different condition for the search bar

--- src/App.js
+++ src/App.js
@@ -2,6 +2,45 @@ import { useEffect, useState } from 'react'
 import axios from 'axios'
 
 
+const CountryName = (props) => {
+  let returnedCountries = props.completeList.filter(country => country.name.common.toLowerCase().includes(props.searchFilter.toLowerCase()))
+
+  if (props.searchFilter.length == 0){
+    return (
+      <p>Type down the country that you want to learn more about!</p>
+    )
+  }
+
+  if (returnedCountries.length > 10) {
+    return (
+         <p>Too many matches, specify another filter</p>
+    )
+  }
+
+  if (returnedCountries.length > 1 && returnedCountries.length < 10 ) {
+    return(
+      <div>
+        {returnedCountries.map(country => {
+          return(
+            <li>
+              <p>{country.name.common}</p>
+            </li>
+          )
+        })}
+      </div>
+    )
+  }
+}
+
+const SearchFilter = (props) => {
+  return(
+    <div>
+        <label htmlFor="search-field">find countries</label>
+        <input id='search-field' value={props.searchField} onChange={props.handleSearchFieldChange} />
+      </div>
+  )
+}
+
 const App = () => {
 
   const [countries, setCountries] = useState([])
@@ -20,7 +59,8 @@ const App = () => {
 
   return (
     <div>
-      <p>search countries:</p> <input value={searchField} onChange={handleSearchFieldChange}></input>
+      <SearchFilter searchField = {searchField} handleSearchFieldChange = {handleSearchFieldChange} />
+      <CountryName searchFilter = {searchField} completeList = {countries} /> 
     </div>
   )
 
